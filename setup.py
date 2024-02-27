98931a7f5e0028a9cbcb6e53697d7866a584b48a
joshnankivel <jnankivel@apple.com>
2024-02-26 16:34:42 -0800

    cookiecutter project skeleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Chess board state detection and move recommendation system',
+    author='Alpha Vision + Team',
+    license='MIT',
+)
