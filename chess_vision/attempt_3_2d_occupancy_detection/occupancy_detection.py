04956f49b19ce9f196a2c0f1567b020f5d207a46
joshnankivel <jnankivel@apple.com>
2024-04-20 06:37:09 -0700

    2d occupancy detection bugfix

--- chess_vision/attempt_3_2d_occupancy_detection/occupancy_detection.py
+++ chess_vision/attempt_3_2d_occupancy_detection/occupancy_detection.py
@@ -109,10 +109,9 @@ if __name__ == "__main__":
     directory = "data/raw/"
     for filename in os.listdir(directory):
         if filename.endswith('.jpeg'):
-            # Full path to the file
             path_image = os.path.join(directory, filename)
-    detect_occupancy(path_image,
-                     output_dir="data/occupancy_detection/",
-                     threshold=15.0,
-                     output_dir_squares="data/occupancy_detection_squares/"
-                     )
+            detect_occupancy(path_image,
+                             output_dir="data/occupancy_detection/",
+                             threshold=15.0,
+                             output_dir_squares="data/occupancy_detection_squares/"
+                             )
