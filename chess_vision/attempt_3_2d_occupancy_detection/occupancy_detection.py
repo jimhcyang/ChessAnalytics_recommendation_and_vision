31d7219d01cd840fcec4bece2dc3a91ff9a2daff
joshnankivel <jnankivel@apple.com>
2024-04-20 12:01:46 -0700

    experiment

--- chess_vision/attempt_3_2d_occupancy_detection/occupancy_detection.py
+++ chess_vision/attempt_3_2d_occupancy_detection/occupancy_detection.py
@@ -1,11 +1,16 @@
+import logging
 import os
 from src.occupancy import detect_occupancy
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(name)s - %(levelname)s - %(message)s')
 
 if __name__ == "__main__":
     directory = "data/raw/"
     for filename in os.listdir(directory):
         if filename.endswith('.jpeg'):
             path_image = os.path.join(directory, filename)
+            logging.info(f"processing {filename}")
             detect_occupancy(path_image,
                              threshold=15.0
                              )
